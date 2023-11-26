82cb92eee69f576fae2279b5109d3e5b00f8f3e7
lorlouis <louis.sven@gmail.com>
2023-11-26 09:41:54 -0500

    added the test.h file I forgot

---
+++ tests/tests.h
@@ -0,0 +1,43 @@
+#ifndef TESTS_H
+#define TESTS_H 1
+
+#include <stdio.h>
+#include <xalloc.h>
+
+#define STRINGIFY(s) XSTRINGIFY(s)
+#define XSTRINGIFY(s) #s
+
+#define TESTS_START \
+int main() { \
+    int test_count = 0; \
+    int failed_tests_count = 0; \
+    int status = 0;
+
+#define TEST_DEF(name) \
+    { \
+        char *test_name = STRINGIFY(name); \
+        test_count += 1; \
+        status = 0; \
+        printf("\033[0;33mRunning\033[0m %s", test_name);
+
+#define ASSERT(condition) \
+        if(!status && !(condition)) { \
+            status = 1; \
+            printf("\n\t%s:%d: `%s` is false", __FILE__, __LINE__, STRINGIFY(condition)); \
+        }
+
+#define TEST_ENDDEF \
+        if(status) { \
+            puts("\t\033[0;31mFAIL\033[0m"); \
+            failed_tests_count += 1; \
+        } else { \
+            puts("\t\033[0;32mPASS\033[0m"); \
+        } \
+    }
+
+#define TESTS_END \
+    printf("\n\tRan %d tests\t%d Failed\n", test_count, failed_tests_count); \
+    return failed_tests_count != 0; \
+}
+
+#endif
